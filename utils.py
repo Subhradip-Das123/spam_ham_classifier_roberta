0066e2a701d2317c3a5002a020cc6dd0505665ac
Subhradip-Das123 <124434372+Subhradip-Das123@users.noreply.github.com>
2025-12-22 14:30:52 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 import torch
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
 MODEL_ID = "subhradip-nlp-labs/roberta-spam-classifier"
 
 @torch.inference_mode()
 def load_model():
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        MODEL_ID,
+        ignore_mismatched_sizes=True  # 🔥 FIX
+    )
+
     classifier = pipeline(
         "text-classification",
-        model=MODEL_ID,
-        tokenizer=MODEL_ID,
-        framework="pt",   # 🔥 force PyTorch
-        device=-1         # 🔥 CPU only
+        model=model,
+        tokenizer=tokenizer,
+        device=-1
     )
     return classifier
-
-
