6aba8b07561997c8862036f77dd3768d1a850d62
Subhradip-Das123 <124434372+Subhradip-Das123@users.noreply.github.com>
2025-12-22 14:28:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,13 +5,12 @@ MODEL_ID = "subhradip-nlp-labs/roberta-spam-classifier"
 
 @torch.inference_mode()
 def load_model():
-    device = 0 if torch.cuda.is_available() else -1
-
     classifier = pipeline(
         "text-classification",
         model=MODEL_ID,
         tokenizer=MODEL_ID,
-        device=device
+        framework="pt",   # 🔥 force PyTorch
+        device=-1         # 🔥 CPU only
     )
     return classifier
 
